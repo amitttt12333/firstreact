2888ca31a88bcad17065a10541754d6ce32b4ce6
Amit <samratamit671@gmail.com>
2021-12-28 20:34:24 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <Home />
+    <Form />
+    </>
+  );
+}
+
+const Home = () =>{
+  return(
+    <>
+    <h1>Hello world</h1>
+      <b>
+        Bold words
+      </b>
+    <i>
+      Italic words
+    </i>
+    </>
+  );
+}
+
+const Form = () =>{
+  return (
+    <>
+    <div>
+      <form>
+        <label htmlFor='email'>Email: </label>
+        <input type="email" id='email'/>
+        <br />
+        <label htmlFor='password'>Password: </label>
+        <input type="password" id='email'/>
+        <br/>
+        <button type='submit'>Submit</button>
+      </form>
+
+    </div>
+
+    </>
+  ) 
+}
+export default App;
